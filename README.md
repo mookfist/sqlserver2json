7c54e0f81d0b964edaeb79725d556fd4f8886288
alex-dow <adow@psikonc.om>
2015-12-31 16:16:06 -0500

    better code

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Very simple script to convert a SQL server database to json files, one file per table. Probably not going to be great with very large tables.
 
+# json output
+
+Attempst to find the primary key of the table and if it can, will use its value as a key in an object literal.
+
+If it can't find the primary key, then each row will be in an array rather than an object.
+
 ## Installation
 
 ```
@@ -11,3 +17,6 @@ $ chmod u+x bin/sqlserver2json.js
 $ bin/sqlserver2json.js --help
 ```
 
+## Testing
+
+You can use --test to do a syntax check on the json files. This is memory intensive. Tables larger than 4gb might cause problems.
--- bin/sqlserver2json.js
+++ bin/sqlserver2json.js
@@ -7,7 +7,8 @@ var clc       = require('cli-color');
 var Q         = require('q');
 var ansi      = require('ansi-escape-sequences');
 var mkdirp    = require('mkdirp');
-
+var jsonlint  = require('jsonlint');
+var spawn     = require('child-process-promise').spawn;
 Q.longStackSupport = true;
 
 var doc = "Usage: sqlserver2json [options]\n"
@@ -17,220 +18,364 @@ var doc = "Usage: sqlserver2json [options]\n"
         + "  --password PASS    SQL Server password\n"
         + "  --database DBNAME  Database name\n"
         + "  --server   SERVER  Database host or IP address\n"
-        + "  --port     PORT    Database port number\n";
-        + "  --output   DIR     Directory to store json files\n";
+        + "  --port     PORT    Database port number\n"
+        + "  --output   DIR     Directory to store json files\n"
+        + "  --simple           Remove graphical widgets\n"
+        + "  --timeout  SECS    Number of seconds to timeout\n"
+        + "  --table    NAME    Dump a specific table name\n";
+        + "  --test             Test JSON files for syntax errors after they are made\n";
 
 if (argv['h'] === true || argv['help'] === true) {
   console.log(doc);
   process.exit(0);
 }
 
+if (argv['timeout']) {
+  argv['connectionTimeout'] = parseInt(argv['timeout']) * 1000;
+  argv['requestTimeout'] = parseInt(argv['timeout']) * 1000;
+}
+
 var data = {};
 
-var progressBar = new CLI.Progress(30);
-var progressLine = new CLI.Line();
-
-var overallProgressBar = new CLI.Progress(30);
-var overallProgressLine = new CLI.Line();
-overallProgressBar.padding = 2;
-
-var tableProgressBar = new CLI.Progress(30);
-var tableProgressLine = new CLI.Line();
-tableProgressBar.padding = 2;
+var prevReadProgress = 0;
+var prevWriteProgress = 0;
+var prevOverallProgress = 0;
 
 function drawProgress(progress) {
-  tableProgress = Math.ceil(progress.progress * 100.0);
-  overallProgress = Math.ceil(progress.overallProgress * 100.0);
+  if (progress === null) {
+    return;
+  }
+  var readProgress = Math.ceil(progress.readProgress * 100.0);
+  var writeProgress = Math.ceil(progress.writeProgress * 100.0);
+  var overallProgress = Math.ceil(progress.progress * 100.0);
 
-  process.stdout.write(ansi.cursor.up(2));
+  overallProgress = Math.ceil(progress.progress * 100.0);
+
+  if (readProgress == prevReadProgress && overallProgress == prevOverallProgress && writeProgress == prevWriteProgress) {
+    return;
+  }
+
+  prevOverallProgress = overallProgress;
+  prevReadProgress = readProgress;
+  prevWriteProgress = writeProgress;
+
+  if (argv['simple'] === true) {
+    console.log('Processing ' + progress.table + ' - ' + progress.msg + ' - r:' + readProgress + '%/w:' + writeProgress + '%/o:' + overallProgress + '%');
+    return;
+  }
+
+  process.stdout.write(ansi.cursor.up(3));
 
   var overallProgressBar = new CLI.Progress(30);
   var overallProgressLine = new CLI.Line();
   overallProgressBar.padding = 2;
 
-  var tableProgressBar = new CLI.Progress(30);
-  var tableProgressLine = new CLI.Line();
-  tableProgressBar.padding = 2;
+  var rowReadingBar = new CLI.Progress(30);
+  var rowReadingLine = new CLI.Line();
+  rowReadingBar.padding = 2;
 
-  overallProgressLine.column(progress.tableName, 30, [clc.cyan]);
+  var rowWritingBar = new CLI.Progress(30);
+  var rowWritingLine = new CLI.Line();
+  rowWritingBar.padding = 2;
+
+  overallProgressLine.column(progress.table + ' - ' + progress.msg, 50, [clc.cyan]);
   overallProgressLine.column(overallProgressBar.update(overallProgress, 100));
   overallProgressLine.fill();
   overallProgressLine.output();
 
-  tableProgressLine.column(progress.msg, 30, [clc.cyan]);
-  tableProgressLine.column(tableProgressBar.update(tableProgress, 100));
-  tableProgressLine.fill();
-  tableProgressLine.output();
+  rowReadingLine.column('Reading', 50, [clc.cyan]);
+  rowReadingLine.column(rowReadingBar.update(readProgress, 100));
+  rowReadingLine.fill();
+  rowReadingLine.output();
+
+  rowWritingLine.column('Writing', 50, [clc.cyan]);
+  rowWritingLine.column(rowWritingBar.update(writeProgress, 100));
+  rowWritingLine.fill();
+  rowWritingLine.output();
 }
 
-var spinner = new CLI.Spinner();
+// var spinner = new CLI.Spinner();
 
-function getTables() {
-  var deferred = Q.defer();
-  spinner.start();
-  spinner.message('Fetching list of tables');
-  var query = "SELECT * FROM sysobjects WHERE xtype='U' ";
-  var request = new sqlserver.Request();
-  Q.ninvoke(request, "query", query)
-  .then(function(recordset) {
-    spinner.stop();
-    console.log('Fetched list of tables');
-    deferred.resolve(recordset);
+function simpleCmd(msg, cb) {
+  var spinner;
+  if (argv['simple'] !== true) {
+    spinner = new CLI.Spinner(msg + ' ... ');
+    spinner.start();
+  } else {
+    process.stdout.write(msg + ' ... ');
+  }
+
+  var defer = Q.defer();
+
+  return Q.fcall(cb)
+  .then(function(result) {
+    if (argv['simple'] !== true) {
+      spinner.stop();
+      process.stdout.write(msg + ' ... done!\n');
+    } else {
+      process.stdout.write('done!\n');
+    }
+    return result; 
+    defer.resolve.apply(defer, arguments);
   })
-  .catch(function(err) {
-    spinner.stop();
-    deferred.reject(err);
+  .fail(function(err) {
+    
+    if (argv['simple'] !== true) {
+      spinner.stop();
+      process.stdout.write(msg + ' ... failed!\n');
+    } else {
+      process.stdout.write('failed!\n');
+    }
+
+    throw err;
   });
-  return deferred.promise;
+
+  return defer.promise;
 }
 
-function getData(tableName) {
-  var deferred = Q.defer();
-  
-  var query = "SELECT * FROM " + tableName;
-  var request = new sqlserver.Request();
-  Q.ninvoke(request, "query", query)
-  .then(function(recordset) {
-   deferred.resolve(recordset);
+function getTables(options) {
+  return simpleCmd('Feching list of tables', function() {
+    if (options['table']) {
+      return Q.fcall(function() {
+        return [[{name: options['table']}]];
+      });
+    } else {
+      var query = "SELECT * FROM sysobjects WHERE xtype='U'";
+      var request = new sqlserver.Request();
+      return Q.ninvoke(request, "query", query);
+    }
+  }).then(function(records) {
+    return records[0];
   });
-  return deferred.promise;
-} 
+}
+
+function connect(config) {
+  return simpleCmd('Connecting to database', function() {
+    return Q.ninvoke(sqlserver, 'connect', config);
+  });
+}
+
+function disconnect(options) {
+  return simpleCmd('Disconnecting from database', function() {
+    return Q.ninvoke(sqlserver, "close");
+  });
+}
 
 function getTableCount(tableName) {
   var query = 'SELECT COUNT(*) as totalRows FROM ' + tableName;
   var request = new sqlserver.Request();
-  return request.query(query);
+  return Q.ninvoke(request, 'query', query)
+  .then(function(results) {
+    return results[0][0]['totalRows'];
+  });
 }
 
-function connect(config) {
-  var deferred = Q.defer();
-  spinner.start();
-  spinner.message('Connecting to database');
-  Q.ninvoke(sqlserver, "connect", config)
-  .then(function() {
-    spinner.stop();
-    console.log('Connected to database');
-    deferred.resolve();
-  })
-  .catch(function(err) {
-    spinner.stop();
-    deferred.reject(err);
+function getPrimaryKey(tableName) {
+  var query = 'SELECT COLUMN_NAME '
+            + 'FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE '
+            + 'WHERE OBJECTPROPERTY(OBJECT_ID(CONSTRAINT_SCHEMA + \'.\' + CONSTRAINT_NAME), \'IsPrimaryKey\') = 1 '
+            + 'AND TABLE_NAME = \'' + tableName + '\'';
+  var request = new sqlserver.Request();
+  return Q.ninvoke(request, 'query', query)
+  .then(function(recordset) {
+    var keys = [];
+    recordset[0].forEach(function(row) {
+      keys.push(row['COLUMN_NAME']);
+    });
+    return keys;
   });
-
-  return deferred.promise;
-}
-
-function disconnect(options) {
-  var deferred = Q.defer();
-  spinner.start();
-  spinner.message('Disconnecting from database');
-  Q.ninvoke(sqlserver, "close")
-  .then(function() {
-    spinner.stop();
-    console.log('Disconnected from database');
-    deferred.resolve();
-  })
-  .catch(function(err) {
-    spinner.stop();
-    deferred.reject(err);
-  });
-
-  return deferred.promise;
 }
 
 function processTable(options, tableName) {
   var deferred = Q.defer();
-  var totalRows = 0;
- 
-  deferred.notify({msg: 'Counting rows', amt: 0.0});
+  var jsonFileName = options['output'] + '/' + tableName + '.json';
+  
+  deferred.notify({table: tableName, msg: 'counting rows', progress: 0});
+
+  var rowCounter = 0;
+  var tableCount = 0;
   getTableCount(tableName)
-  .then(function(rows) {
-    totalRows = rows[0]['totalRows'];
-    deferred.notify({msg: 'Fetching data', amt: 0.0});
-    return getData(tableName);
-  })
-  .then(function(records) {
-    var writerDeferred = Q.defer();
-
-    var jsonFileName = options['output'] + '/' + tableName + '.json';
-
-    var wstream = fs.createWriteStream(jsonFileName);
-
-    wstream.on('finish', function() {
-      writerDeferred.resolve()
-    });
-
-    wstream.write('[{\n');
-    recordCounter = 0;
-    records[0].forEach(function(record) {
-      var keys = [];
-      Object.keys(record).forEach(function(key) {
-        keys.push('  "' + key + '": "' + record[key] + '"');
-      });
-      wstream.write(keys.join(',\n'));
-      recordCounter++;
-      
-      if (recordCounter < records.length) {
-        wstream.write('},{');
-      }
-      deferred.notify({msg: 'Writing Data', amt: (recordCounter / records[0].length)});
-    });
-
-    wstream.end('}]');
-
-    return writerDeferred;
-  })
   .then(function() {
-    deferred.resolve();
-  });
+    tableCount = arguments[0];
+    deferred.notify({table: tableName, msg: 'getting primary key', progress: 0});
+    return getPrimaryKey(tableName);
+  })
+  .then(function(pk) {
+    var rowDeferral = Q.defer();
+    var rowCounter = 0;
+    var writerCounter = 0;
+    var request = new sqlserver.Request();
+    request.stream = true;
 
+    var writeStream = fs.createWriteStream(jsonFileName);
+    writeStream.on('close', function() {
+      rowDeferral.resolve();
+    });
+
+    if (pk.length > 1) {
+      writeStream.write('[{\n');
+    } else {
+      writeStream.write('{\n');
+    }
+
+    request.query('select * from ' + tableName);
+
+    var indent = 0;
+    var indentAmt = 2;
+
+    var writtenKeys = {};
+    request.on('row', function(row) {
+      indent = indent + indentAmt;
+      if (pk.length == 1) {
+        var pkValue = row[pk[0]];
+        writeStream.write(' '.repeat(indent) + '"' + pkValue + '": {\n');
+        indent = indent + indentAmt;
+      }
+
+      var keys = [];
+      
+      Object.keys(row).forEach(function(key) {
+        var value = '';
+        if (typeof row[key] == "string") {
+          value = row[key].replace(/\n/g, '\\n');
+          value = value.replace(/\r/g, '\\r');
+          value = value.replace(/\t/g, '\\t');
+          value = '"' + value.replace(/"/g, '\\"').trim() + '"';
+          
+        } else {
+          value = row[key];
+        }
+        keys.push(' '.repeat(indent) + '"' + key + '": ' + value);
+      });
+      var column = keys.join(',\n');
+      writeStream.write(column, function() {
+        writerCounter++;
+        deferred.notify({table: tableName, msg: 'Written row', writeProgress: (writerCounter / tableCount), readProgress: (rowCounter / tableCount)});
+      });
+      
+      rowCounter++;
+      if (rowCounter != tableCount) {
+        indent = indent - indentAmt;
+        if (pk.length == 1) {
+          writeStream.write('\n' + ' '.repeat(indent) + '},\n');
+        } else {
+          writeStream.write('\n' + ' '.repeat(indent) + '},{\n');
+        }
+      }
+      deferred.notify({table: tableName, msg: 'Reading row', writeProgress: (writerCounter / tableCount), readProgress: (rowCounter / tableCount)});
+
+      if (pk.length == 1) {
+        indent = indent - indentAmt;
+      }
+    });
+
+    request.on('done', function(row) {
+      if (pk.length == 1) {
+        writeStream.end('\n' + ' '.repeat(indent) + '}\n}');
+      } else {
+        writeStream.end('\n}]');
+      }
+    });
+
+    return rowDeferral.promise;
+  }).then(function() {
+    deferred.resolve();
+  }).fail(function(err) {
+    deferred.reject(err);
+  });
   return deferred.promise;
 }
 
+var totalTables, tableCounter;
+
 function processTables(options, tables) {
+  console.log('\n\n\n');
   var deferred = Q.defer();
-  var totalTables = tables[0].length;
-  var tableCounter = 0;
 
-  var promises = [];
-
-  tables[0].forEach(function(table) {
-    var promise = processTable(options, table.name)
-    .then(function() {
-      tableCounter = tableCounter + 1;
-    })
-    .progress(function(progress) {
-      deferred.notify({tableName: table.name, progress: progress.amt, msg: progress.msg, overallProgress: (tableCounter / totalTables)});
-    })
-
-    promises.push(promise);
+  if (!totalTables) {
+    totalTables = tables.length;
+    tableCounter = 0;
+  }
+  
+  var result = Q();
+  tables.forEach(function(table) {
+    result = result.then(processTable.bind(null, options, table.name)).then(function() {
+      tableCounter++;
+    });
   });
+  
+  return result.progress(function(progress) {
+    if (!progress['readProgress']) {
+      progress['readProgress'] = 0;
+    }
 
-  Q.all(promises)
-  .then(function() {
-    deferred.resolve();
+    if (!progress['writeProgress']) {
+      progress['writeProgress'] = 0;
+    }
+
+    progress['progress'] = tableCounter / totalTables;
+    return progress;
   });
-  return deferred.promise;
 }
 
 function makeDirs(dir) {
   return Q.nfcall(mkdirp, dir);
 }
 
+function testJsonFile(fn) {
+  return simpleCmd('Testing ' + fn, function() {
+    var args = [
+      '--max-old-space-size=4096',
+      'node_modules/jsonlint/lib/cli.js',
+      fn
+    ];
+    return spawn('node', args, { capture: ['stdout', 'stderr']})
+    .progress(function() {
+      return null;
+    });
+  });
+}
+
+function testJsonFiles(dir) {
+  return Q.nfcall(fs.readdir, dir)
+  .then(function(results) {
+    var result = Q();
+    results.forEach(function(fn) {
+      result = result.then(function() {
+        return testJsonFile(dir + '/' + fn);
+      });
+    });
+    return result;
+  });
+};
+
+console.log('/------------------------------------------------\\');
+console.log('| sqlserver2json                                 |');
+console.log('| if you have very large tables, then go to the  |');
+console.log('| pub, it will be a while.                       |');
+console.log('\\------------------------------------------------/');
+console.log(' ');
+
 makeDirs(argv['output'])
 .then(connect.bind(null, argv))
 .then(getTables.bind(null, argv))
 .then(processTables.bind(null, argv))
+.then(function() {
+  if (argv['test'] == true) {
+    if (argv['table']) {
+      return testJsonFile(argv['output'] + '/' + argv['table'] + '.json');
+    } else {
+      return testJsonFiles(argv['output']);
+    }
+  }
+})
 .then(disconnect.bind(null, argv))
 .progress(drawProgress)
-.fail(function() {
-  sqlserver.close();
+.fail(function(err) {
   console.log('== ERROR == ERROR == ERROR == ERROR == ERROR == ERROR ==');
-  console.dir(arguments);
-})
-.catch(function() {
-  console.log('== EXCEPTION == EXCEPTION == EXCEPTION == EXCEPTION ==');
-  console.log(arguments);
+  console.dir(err);
+  console.log(err.stack);
 
   sqlserver.close();
-});
+})
+.done();
